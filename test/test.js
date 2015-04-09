5a0468c0363e6b9c6aaf9776570a89d720c10f0e
@zaftzaft <zaftzaft7@gmail.com>
2015-04-09 18:38:31 +0900

    update test

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ vimcs.init({
 }, function(){
   console.log(vimcs("MatchParen")("Str"));
   console.log(vimcs("MatchParen", "bg")("Str"));
-  console.log(vimcs("SpellLocal")("Str"));
+  console.log(vimcs("SpellLocal", "", true)("Str"));
   console.log(vimcs("SpellLocal", "fg")("Str"));
   console.log(vimcs("Todo")("vim TODO color"));
 });
